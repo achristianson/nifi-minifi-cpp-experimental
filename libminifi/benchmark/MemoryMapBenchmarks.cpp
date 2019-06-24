38f3159a3f19dd6413353b76678f5484e96fb774
Andrew I. Christianson <andy@andyic.org>
2019-06-24 19:17:22 -0400

    mmap - add db repo benchmarks

--- libminifi/benchmark/MemoryMapBenchmarks.cpp
+++ libminifi/benchmark/MemoryMapBenchmarks.cpp
@@ -20,45 +20,60 @@
 #include "../test/TestBase.h"
 #include "ResourceClaim.h"
 #include "core/Core.h"
+#include "repository/FileSystemRepository.h"
+#include "repository/VolatileContentRepository.h"
 #include "properties/Configure.h"
+
+#ifdef ENABLE_ROCKSDB_BENCHMARKS
+#include "DatabaseContentRepository.h"
+#endif  // ENABLE_ROCKSDB_BENCHMARKS
 
 template<class T>
 class MemoryMapBMFixture : public benchmark::Fixture {
  public:
   void SetUp(const ::benchmark::State &state) {
-    testController = std::make_shared<TestController>();
-    repo = std::make_shared<T>();
-    repo->initialize(std::make_shared<minifi::Configure>());
+    test_controller_ = std::make_shared<TestController>();
+    repo_ = std::make_shared<T>();
+    conf_ = std::make_shared<minifi::Configure>();
     char format[] = "/tmp/testRepo.XXXXXX";
-    auto dir = std::string(testController->createTempDirectory(format));
-    test_file = dir + "/testfile";
-    claim = std::make_shared<minifi::ResourceClaim>(test_file, repo);
+    dir_ = std::string(test_controller_->createTempDirectory(format));
+    test_file_ = dir_ + "/testfile";
+    claim_ = std::make_shared<minifi::ResourceClaim>(test_file_, repo_);
   }
 
   void TearDown(const ::benchmark::State &state) {
   }
 
+  void init_db_repo() {
+    conf_->set(minifi::Configure::nifi_dbcontent_repository_directory_default, dir_);
+    init_repo();
+  }
+
+  void init_repo() {
+    repo_->initialize(conf_);
+  }
+
   void set_test_input(size_t size, char c) {
-    test_string = "";
-    test_string.resize(size, c);
-    auto mm = repo->mmap(claim, test_string.length(), false);
-    mm->resize(test_string.length());
-    memcpy(mm->getData(), &test_string[0], test_string.length());
+    test_string_ = "";
+    test_string_.resize(size, c);
+    auto mm = repo_->mmap(claim_, test_string_.length(), false);
+    mm->resize(test_string_.length());
+    memcpy(mm->getData(), &test_string_[0], test_string_.length());
   }
 
   void set_test_expected_output(size_t size, char c) {
-    expected_string = "";
-    expected_string.resize(size, c);
+    expected_string_ = "";
+    expected_string_.resize(size, c);
   }
 
   void validate_string(const char *read_string) {
-    if (strncmp(read_string, expected_string.c_str(), expected_string.length()) != 0) {
+    if (strncmp(read_string, expected_string_.c_str(), expected_string_.length()) != 0) {
       throw std::runtime_error("string read failed");
     }
   }
 
   void validate_byte(size_t pos, const char b) {
-    if (b != expected_string[pos]) {
+    if (b != expected_string_[pos]) {
       throw std::runtime_error("byte read failed");
     }
   }
@@ -70,68 +85,83 @@ class MemoryMapBMFixture : public benchmark::Fixture {
   std::vector<size_t> random_points() {
     std::vector<size_t> p;
 
-    for (size_t i = 0; i < test_string.length() / 2; i += test_string.length() / 100) {
+    for (size_t i = 0; i < test_string_.length() / 2; i += test_string_.length() / 100) {
       p.push_back(i);
-      p.push_back(test_string.length() - 1);
+      p.push_back(test_string_.length() - 1);
     }
 
     return p;
   }
 
-  std::shared_ptr<TestController> testController;
-  std::shared_ptr<T> repo;
-  std::shared_ptr<minifi::ResourceClaim> claim;
-  std::string test_file;
-  std::string test_string;
-  std::string expected_string;
+  std::shared_ptr<minifi::Configure> conf_;
+  std::shared_ptr<TestController> test_controller_;
+  std::shared_ptr<T> repo_;
+  std::shared_ptr<minifi::ResourceClaim> claim_;
+  std::string test_file_;
+  std::string test_string_;
+  std::string expected_string_;
+  std::string dir_;
 };
 
 typedef MemoryMapBMFixture<core::repository::FileSystemRepository> FSMemoryMapBMFixture;
 typedef MemoryMapBMFixture<core::repository::VolatileContentRepository> VolatileMemoryMapBMFixture;
 
+#ifdef ENABLE_ROCKSDB_BENCHMARKS 
+typedef MemoryMapBMFixture<core::repository::DatabaseContentRepository> DatabaseMemoryMapBMFixture;
+#endif  // ENABLE_ROCKSDB_BENCHMARKS
+
 template<class T>
-void mmap_read(T *fixture, benchmark::State &st, bool read_only) {
+void mmap_read(T *fixture, benchmark::State &st) {
   while (st.KeepRunning()) {
-    auto mm = fixture->repo->mmap(fixture->claim, fixture->test_string.length(), read_only);
+    auto mm = fixture->repo_->mmap(fixture->claim_, fixture->test_string_.length(), true);
     fixture->validate_string(reinterpret_cast<const char *>(mm->getData()));
   }
+
+  fixture->repo_->stop();
 }
 
 template<class T>
 void mmap_read_random(T *fixture, benchmark::State &st) {
   auto r = fixture->random_points();
-  auto mm = fixture->repo->mmap(fixture->claim, fixture->test_string.length(), true);
+  auto mm = fixture->repo_->mmap(fixture->claim_, fixture->test_string_.length(), true);
   while (st.KeepRunning()) {
     auto data = reinterpret_cast<char *>(mm->getData());
     for (size_t p : r) {
       fixture->validate_byte(p, data[p]);
     }
   }
+
+  fixture->repo_->stop();
 }
 
 template<class T>
 void mmap_write_read(T *fixture, benchmark::State &st) {
   while (st.KeepRunning()) {
-    auto mm = fixture->repo->mmap(fixture->claim, fixture->test_string.length(), false);
-    memcpy(mm->getData(), &(fixture->expected_string[0]), fixture->test_string.length());
+    fixture->repo_->remove(fixture->claim_);
+    auto mm = fixture->repo_->mmap(fixture->claim_, fixture->test_string_.length(), false);
+    memcpy(mm->getData(), &(fixture->expected_string_[0]), fixture->test_string_.length());
     fixture->validate_string(reinterpret_cast<const char *>(mm->getData()));
   }
+
+  fixture->repo_->stop();
 }
 
 template<class T>
 void cb_read(T *fixture, benchmark::State &st) {
   while (st.KeepRunning()) {
-    auto rs = fixture->repo->read(fixture->claim);
+    auto rs = fixture->repo_->read(fixture->claim_);
     std::vector<uint8_t> buf;
-    rs->readData(buf, fixture->test_string.length() + 1);
+    rs->readData(buf, fixture->test_string_.length() + 1);
     fixture->validate_string(reinterpret_cast<const char *>(&buf[0]));
   }
+
+  fixture->repo_->stop();
 }
 
 template<class T>
 void cb_read_random(T *fixture, benchmark::State &st) {
   auto r = fixture->random_points();
-  auto rs = fixture->repo->read(fixture->claim);
+  auto rs = fixture->repo_->read(fixture->claim_);
   while (st.KeepRunning()) {
     for (size_t p : r) {
       rs->seek(p);
@@ -140,209 +170,310 @@ void cb_read_random(T *fixture, benchmark::State &st) {
       fixture->validate_byte(p, b);
     }
   }
+
+  fixture->repo_->stop();
 }
 
 template<class T>
 void cb_write_read(T *fixture, benchmark::State &st) {
   while (st.KeepRunning()) {
     {
-      auto ws = fixture->repo->write(fixture->claim, false);
-      ws->write(reinterpret_cast<uint8_t *>(&(fixture->expected_string[0])), fixture->test_string.length());
+      fixture->repo_->remove(fixture->claim_);
+      auto ws = fixture->repo_->write(fixture->claim_, false);
+      ws->write(reinterpret_cast<uint8_t *>(&(fixture->expected_string_[0])), fixture->test_string_.length());
     }
 
-    auto rs = fixture->repo->read(fixture->claim);
+    auto rs = fixture->repo_->read(fixture->claim_);
     std::vector<uint8_t> buf;
-    rs->readData(buf, fixture->test_string.length() + 1);
+    rs->readData(buf, fixture->test_string_.length() + 1);
     fixture->validate_string(reinterpret_cast<const char *>(&buf[0]));
   }
+
+  fixture->repo_->stop();
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_Read_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'x');
-  mmap_read<FSMemoryMapBMFixture>(this, st, false);
-}
-
-BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_Read_RO_Tiny)(benchmark::State &st) {
-  set_test_input(10, 'x');
-  set_test_expected_output(10, 'x');
-  mmap_read<FSMemoryMapBMFixture>(this, st, true);
+  mmap_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, Callback_FileSystemRepository_Read_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'x');
   cb_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_WriteRead_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'y');
   mmap_write_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, Callback_FileSystemRepository_WriteRead_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'y');
   cb_write_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_Read_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'x');
-  mmap_read<FSMemoryMapBMFixture>(this, st, false);
-}
-
-BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_Read_RO_Small)(benchmark::State &st) {
-  set_test_input(131072, 'x');
-  set_test_expected_output(131072, 'x');
-  mmap_read<FSMemoryMapBMFixture>(this, st, true);
+  mmap_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, Callback_FileSystemRepository_Read_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'x');
   cb_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_WriteRead_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'y');
   mmap_write_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, Callback_FileSystemRepository_WriteRead_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'y');
   cb_write_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_Read_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'x');
-  mmap_read<FSMemoryMapBMFixture>(this, st, false);
-}
-
-BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_Read_RO_Large)(benchmark::State &st) {
-  set_test_input(33554432, 'x');
-  set_test_expected_output(33554432, 'x');
-  mmap_read<FSMemoryMapBMFixture>(this, st, true);
+  mmap_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, Callback_FileSystemRepository_Read_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'x');
   cb_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_WriteRead_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'y');
   mmap_write_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, Callback_FileSystemRepository_WriteRead_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'y');
   cb_write_read<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, MemoryMap_FileSystemRepository_RandomRead_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'x');
   mmap_read_random<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(FSMemoryMapBMFixture, Callback_FileSystemRepository_RandomRead_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'x');
   cb_read_random<FSMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, MemoryMap_VolatileRepository_Read_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'x');
-  mmap_read<VolatileMemoryMapBMFixture>(this, st, false);
+  mmap_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, Callback_VolatileRepository_Read_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'x');
   cb_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, MemoryMap_VolatileRepository_WriteRead_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'y');
   mmap_write_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, Callback_VolatileRepository_WriteRead_Tiny)(benchmark::State &st) {
+  init_repo();
   set_test_input(10, 'x');
   set_test_expected_output(10, 'y');
   cb_write_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, MemoryMap_VolatileRepository_Read_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'x');
-  mmap_read<VolatileMemoryMapBMFixture>(this, st, false);
+  mmap_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, Callback_VolatileRepository_Read_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'x');
   cb_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, MemoryMap_VolatileRepository_WriteRead_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'y');
   mmap_write_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, Callback_VolatileRepository_WriteRead_Small)(benchmark::State &st) {
+  init_repo();
   set_test_input(131072, 'x');
   set_test_expected_output(131072, 'y');
   cb_write_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, MemoryMap_VolatileRepository_Read_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'x');
-  mmap_read<VolatileMemoryMapBMFixture>(this, st, false);
+  mmap_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, Callback_VolatileRepository_Read_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'x');
   cb_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, MemoryMap_VolatileRepository_WriteRead_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'y');
   mmap_write_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
 BENCHMARK_F(VolatileMemoryMapBMFixture, Callback_VolatileRepository_WriteRead_Large)(benchmark::State &st) {
+  init_repo();
   set_test_input(33554432, 'x');
   set_test_expected_output(33554432, 'y');
   cb_write_read<VolatileMemoryMapBMFixture>(this, st);
 }
 
-BENCHMARK_F(VolatileMemoryMapBMFixture, MemoryMap_VolatileRepository_RandomRead_Large)(benchmark::State &st) {
-  set_test_input(33554432, 'x');
-  set_test_expected_output(33554432, 'x');
-  mmap_read_random<VolatileMemoryMapBMFixture>(this, st);
+#ifdef ENABLE_ROCKSDB_BENCHMARKS 
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, MemoryMap_DatabaseRepository_Read_Tiny)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(10, 'x');
+  set_test_expected_output(10, 'x');
+  mmap_read<DatabaseMemoryMapBMFixture>(this, st);
 }
 
-BENCHMARK_F(VolatileMemoryMapBMFixture, Callback_VolatileRepository_RandomRead_Large)(benchmark::State &st) {
-  set_test_input(33554432, 'x');
-  set_test_expected_output(33554432, 'x');
-  cb_read_random<VolatileMemoryMapBMFixture>(this, st);
+BENCHMARK_F(DatabaseMemoryMapBMFixture, Callback_DatabaseRepository_Read_Tiny)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(10, 'x');
+  set_test_expected_output(10, 'x');
+  cb_read<DatabaseMemoryMapBMFixture>(this, st);
 }
 
-//more tests:
-// duplicate all tests for volatile, db repos
+BENCHMARK_F(DatabaseMemoryMapBMFixture, MemoryMap_DatabaseRepository_WriteRead_Tiny)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(10, 'x');
+  set_test_expected_output(10, 'y');
+  mmap_write_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, Callback_DatabaseRepository_WriteRead_Tiny)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(10, 'x');
+  set_test_expected_output(10, 'y');
+  cb_write_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, MemoryMap_DatabaseRepository_Read_Small)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(131072, 'x');
+  set_test_expected_output(131072, 'x');
+  mmap_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, Callback_DatabaseRepository_Read_Small)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(131072, 'x');
+  set_test_expected_output(131072, 'x');
+  cb_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, MemoryMap_DatabaseRepository_WriteRead_Small)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(131072, 'x');
+  set_test_expected_output(131072, 'y');
+  mmap_write_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, Callback_DatabaseRepository_WriteRead_Small)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(131072, 'x');
+  set_test_expected_output(131072, 'y');
+  cb_write_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, MemoryMap_DatabaseRepository_Read_Large)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(33554432, 'x');
+  set_test_expected_output(33554432, 'x');
+  mmap_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, Callback_DatabaseRepository_Read_Large)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(33554432, 'x');
+  set_test_expected_output(33554432, 'x');
+  cb_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, MemoryMap_DatabaseRepository_WriteRead_Large)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(33554432, 'x');
+  set_test_expected_output(33554432, 'y');
+  mmap_write_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, Callback_DatabaseRepository_WriteRead_Large)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(33554432, 'x');
+  set_test_expected_output(33554432, 'y');
+  cb_write_read<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, MemoryMap_DatabaseRepository_RandomRead_Large)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(33554432, 'x');
+  set_test_expected_output(33554432, 'x');
+  mmap_read_random<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+BENCHMARK_F(DatabaseMemoryMapBMFixture, Callback_DatabaseRepository_RandomRead_Large)(benchmark::State &st) {
+  init_db_repo();
+  set_test_input(33554432, 'x');
+  set_test_expected_output(33554432, 'x');
+  cb_read_random<DatabaseMemoryMapBMFixture>(this, st);
+}
+
+#endif  // ENABLE_ROCKSDB_BENCHMARKS
+
 BENCHMARK_MAIN();
